1c395a0bfbdd552e937a2eca213fc569307c70ab
Tanish Rathi <tanishrathi07@gmail.com>
2026-03-26 11:55:45 -0400

    changed env to contain db info

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -8,4 +8,11 @@ module.exports = {
   internalApiToken: process.env.INTERNAL_API_TOKEN,
   agentJwtSecret: process.env.AGENT_JWT_SECRET,
   adminApiToken: process.env.ADMIN_API_TOKEN,
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || 'KleenUpReferral',
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD,
+  }
 };
