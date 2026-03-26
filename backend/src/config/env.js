a7139c4d640ade27eea9a3ad296dedabd6b95754
Tanish Rathi <tanishrathi07@gmail.com>
2026-03-25 23:55:21 -0400

    db config

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,4 +3,11 @@ module.exports = {
   wpBaseUrl: process.env.WP_BASE_URL,
   wpUsername: process.env.WP_USERNAME,
   wpAppPassword: process.env.WP_APP_PASSWORD,
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || 'KleenUpReferral',
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD,
+  }
 };
