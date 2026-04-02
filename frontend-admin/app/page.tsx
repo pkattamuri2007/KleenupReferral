90d5730ffb29381d1923de4cb32fb42378af8cca
samkakkar <kakkar4@purdue.edu>
2026-04-01 23:48:45 -0400

    final

--- frontend-admin/app/page.tsx
+++ frontend-admin/app/page.tsx
@@ -1,4 +1,5 @@
-import AdminDashboard from '@/components/admin/AdminDashboard'
-export default function Page() {
-  return <AdminDashboard />
+import AdminDashboard from "./components/AdminDashboard";
+
+export default function Home() {
+  return <AdminDashboard />;
 }
